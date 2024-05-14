d057924bb628ef8ccda4222b8eacaf4fb19779f6
luanbt21 <luanbt21@gmail.com>
2024-05-14 17:26:52 +0700

    add git-blame-nvim

--- lua/community.lua
+++ lua/community.lua
@@ -2,10 +2,12 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.color.transparent-nvim" },
-  { import = "astrocommunity.colorscheme.catppuccin", opts = { dim_inactive = true, transparent_background = true } },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+
+  { import = "astrocommunity.git.git-blame-nvim" },
   { import = "astrocommunity.completion.codeium-vim" },
+
   { import = "astrocommunity.pack.rust" },
-  
-  -- { import = "astrocommunity.pack.lua" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.svelte" },
+  { import = "astrocommunity.pack.lua" },
 }
