971c299674022ebf3bed76a8e4ff2a06f66af28a
luanbt21 <luanbt21@gmail.com>
2024-05-19 15:26:30 +0700

    community transparent

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,34 +1,6 @@
 ---@type LazySpec
 return {
   {
-    "xiyaowong/transparent.nvim",
-    lazy = false,
-    config = function()
-      local transparent = require "transparent"
-      transparent.setup {
-        extra_groups = {
-          "NormalFloat",
-          "NvimTreeNormal",
-        },
-      }
-      transparent.clear_prefix "BufferLine"
-      transparent.clear_prefix "NeoTree"
-      transparent.clear_prefix "lualine"
-    end,
-    dependencies = {
-      {
-        "AstroNvim/astrocore",
-        opts = {
-          mappings = {
-            n = {
-              ["<Leader>uT"] = { "<Cmd>TransparentToggle<CR>", desc = "Toggle transparency" },
-            },
-          },
-        },
-      },
-    },
-  },
-  {
     "ray-x/lsp_signature.nvim",
     event = "BufRead",
     config = function() require("lsp_signature").setup() end,
